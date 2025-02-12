ef3ffc2451aefe9a9708044c6f9b4e9ce700a9e9
Vikas8920 <vikasmishra0306@gmail.com>
2025-02-12 11:51:44 +0530

    12-02-2025

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import SelectForm from './Component/SelectForm'
+import React, { useState } from 'react'
+import Form3 from './Component/Form3'
+import Display from './Component/Display'
+// import Checkbox from './Component/Checkbox'
+// import SelectForm from './Component/SelectForm'
+// import Radio from './Component/Radio'
 // import Form2 from './Component/Form2'
 // import Form from './Component/Form'
 
 const App = () => {
+  const [formData, setFormData] = useState(null)
+  const handleFormSubmit = (data) =>{
+    setFormData(data)
+  }
   return (
     <>
      {/* <Form/> */}
      {/* <Form2/> */}
-      <SelectForm/>
+      {/* <SelectForm/> */}
+      {/* <Radio/> */}
+      {/* <Checkbox/> */}
+      <Form3 onFormSubmit={handleFormSubmit}/>
+      {formData && <Display data={formData}/>}
+      {/* if formData exists, render the display component */}
     </>
   )
 }
---
+++ src/Component/Checkbox.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const Checkbox = () => {
+    const[selectedOption, setSelectedOption] = useState({
+        option1: false,
+        option2: false,
+        option3: false
+    })
+    const handleChange = (e)=>{
+        const {name, checked} = e.target   // extract checkbox name and checked states
+        setSelectedOption({...selectedOption, [name]:checked})
+    }
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+        console.log(`Selected Option:`, selectedOption)
+    }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='1'>Option 1</label>
+        <input type='checkbox' name='option1' checked={selectedOption.option1} onChange={handleChange} id='1'/>
+        <br/>
+        <label htmlFor='2'>Option 2</label>
+        <input type='checkbox' name='option2' checked={selectedOption.option2} onChange={handleChange} id='2'/>
+        <br/>
+        <label htmlFor='3'>Option 3</label>
+        <input type='checkbox' name='option3' checked={selectedOption.option3} onChange={handleChange} id='3'/>
+        <br/>
+        <button type='submit'>Submit</button>
+      </form>
+    </>
+  )
+}
+
+export default Checkbox
